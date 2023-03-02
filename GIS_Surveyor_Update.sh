ed173ef911c3227c194b25044c167664a78925c3
Nickolaus Alderete <122177990+Nkalderete@users.noreply.github.com>
2023-03-02 14:14:08 -0600

    Update GIS_Surveyor_Update.sh

--- GIS_Surveyor_Update.sh
+++ GIS_Surveyor_Update.sh
@@ -5,10 +5,6 @@ sudo apt-get update
 
 sudo apt-get upgrade -y
 
-sudo apt-get install -y unattended-upgrades
-
-sudo dpkg-reconfigure -plow unattended-upgrades
-
 sudo apt-get install -y rsync
 
 echo "Updates Complete"
