85ba54e5e5e7803a5fcc608adb0c334465ba5a51
roguione <myjobcontacts@yahoo.com>
2023-03-03 03:10:50 -0600

    Update micdrop.sh

--- micdrop.sh
+++ micdrop.sh
@@ -1,1 +1,88 @@
 
+
+#!/bin/bash
+
+echo -e "\033[1mThank you for attending a Kublacon Presentation!\033[0m"
+
+sleep 2
+
+clear
+
+echo -e "\033[1mThank you for attending a Kublacon Presentation!\033[0m"
+
+sleep 2
+
+clear
+
+echo -e "\033[1mBrought to you by Team KUBLACON\033[0m"
+
+sleep 3
+
+clear
+
+echo "  /^ ^\\"
+echo " / 0 0 \\"
+echo " V\ Y /V"
+echo "  / - \\"
+echo " /    \\"
+echo " \_/\_/"
+echo -e "\033[1mDigDogData\033[0m"
+
+sleep 4
+
+clear
+
+echo -e "\033[1mThank You\033[0m"
+
+sleep 2
+
+echo -e "\033[1mGoodbye!\033[0m"
+
+sleep 2
+
+clear
+
+echo -e "\033[1mYall have yourselve's a DogDATA of a day now\033[0m"
+
+sleep 3
+
+for i in $(seq 1 10)
+do
+    for j in $(seq 1 $i)
+    do
+        echo ""
+    done 
+    echo -e "\033[1mNick w/ 2 J's\033[0m "             
+    echo "  /^ ^\\"
+    echo " / 0 0 \\"
+    echo " V\ Y /V"
+    echo "  / ^ \\"
+    echo " /    \\"
+    echo " \_/\_/" 
+    echo -e "\033[1mSkruppa-lax\033[0m"
+
+    sleep 0.5
+    clear
+done
+
+    echo -e "\033[1m!!!Tm Kublacon OUT w/ THANKS!!!!\033[0M"
+
+    sleep 5
+    clear
+
+    echo -e "\033[1mCREDITS & Thanks to:\033[0m"
+    echo -e "\033[1mHexx & Ethan (GIS)\033[0m"
+    echo -e "\033[1mJustin D & Nick A\033[0m"
+    echo -e "\033[1mEngineer Bob @ Direct Lynx\033[0m"
+    echo -e "\033[1mThis couldn't have Happened\033[0m"
+    echo -e "\033[1mWithout YOU\033[0m"
+
+    sleep 6
+    clear
+
+
+exit
+
+
+
+
